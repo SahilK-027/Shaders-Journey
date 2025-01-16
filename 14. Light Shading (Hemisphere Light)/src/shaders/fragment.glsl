f3c9b9416c3e8c300dbda7b210995655c920770b
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2025-01-16 21:44:07 +0530

    ✨ Add gamma correction

--- 14. Light Shading (Hemisphere Light)/src/shaders/fragment.glsl
+++ 14. Light Shading (Hemisphere Light)/src/shaders/fragment.glsl
@@ -15,23 +15,23 @@ float remap(float v, float inMin, float inMax, float outMin, float outMax) {
 void main() {
     vec3 normal = normalize(vNormal);
     // Base color
-    vec3 baseColor = vec3(0.5);
+    vec3 baseColor = vec3(0.59);
     vec3 lighting = vec3(0.0);
 
     // Ambient light
     vec3 ambientLight = vec3(0.5);
 
     // Hemisphere light
-    vec3 topColor = vec3(0.0, 0.72, 1.0);
-    vec3 bottomColor = vec3(1.0, 0.42, 0.0);
+    vec3 topColor = vec3(0.98, 0.13, 0.0);
+    vec3 bottomColor = vec3(0.0, 0.27, 1.0);
     float hemiMix = remap(normal.y, -1.0, 1.0, 0.0, 1.0);
     vec3 hemiSphereLight = mix(bottomColor, topColor, hemiMix);
 
-    lighting += ambientLight;
+    // lighting += ambientLight;
     lighting += hemiSphereLight;
 
     vec3 color = baseColor * lighting;
-
+    color = pow(color, vec3(1.0/2.2));
     gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
